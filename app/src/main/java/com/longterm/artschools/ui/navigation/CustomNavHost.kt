826e0580f3455cd82924fa69ef5482f54b2d459c
skgrechishnikov <srg.grch@gmail.com>
2023-05-20 23:54:15 +0300

    Icon setup

--- app/src/main/java/com/longterm/artschools/ui/navigation/CustomNavHost.kt
+++ app/src/main/java/com/longterm/artschools/ui/navigation/CustomNavHost.kt
@@ -1,5 +1,7 @@
 package com.longterm.artschools.ui.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -22,4 +24,5 @@ fun CustomNavHost(
 
 fun NavController.navigate(destination: Destination) {
     navigate(destination.name)
+    Icons.Default.Add
 }
